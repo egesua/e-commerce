aed48fd37b7e89e098163f7e7ba99bb7ed8df9a4
egesua <egesu06@gmail.com>
2024-02-07 14:12:37 +0300

    api.js added

---
+++ e-commerce/api/api.js
@@ -0,0 +1,23 @@
+import axios from "axios";
+
+export const axiosWithAuth = () => {
+  const token = localStorage.getItem("token");
+
+  return axios.create({
+    baseURL: "https://workintech-fe-ecommerce.onrender.com/",
+    headers: token
+      ? {
+          authorization: token,
+        }
+      : {},
+  });
+};
+export let API;
+
+export const renewAPI = (token) => {
+  token && localStorage.setItem("token", token);
+
+  API = axiosWithAuth();
+};
+
+renewAPI(localStorage.getItem("token"));
